14b9eb622e41cf595f0cf3d6a040e0705251d9cb
Yuriko Darragh <beljar731@gmail.com>
2020-06-07 10:13:31 +0900

    modified some error

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './styles/App.css';
+import '../styles/App.css';
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
      <Router>
       <Route exact path='/' component={Home}/>
-      <Route path='/About' component={About}/>
      </Router>
     </div>
   );
