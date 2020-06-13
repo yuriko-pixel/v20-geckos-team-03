52fd16dc2f5d54d55e8fa2dc575d0171d955de90
Yuriko Darragh <beljar731@gmail.com>
2020-06-13 14:24:12 +0900

    updated styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,35 +2,40 @@ import React from 'react';
 import Layout from './Layout'
 import {Link} from 'react-router-dom'
 import { ThemeContext } from '../context/ThemeContext'
+import homestyle from '../styles/homestyle.css'
 
 class Home extends React.Component {
   static contextType = ThemeContext;
 
   render() {
     const doughnuts = this.context;
-    console.log(this.context);
+    console.log(doughnuts);
     return (
       <Layout>
         <div className="container">
           <div className="img">
             <span>Doughnut Doughnut Doughnut!</span>
           </div>
-          <p>Doughnut Collections</p>
+          <p className="carttitle">Doughnut Collections</p>
           <div className="cartcontainer">
             <div className="flex">
-            {/* {
-              doughnuts.map(item=>
+            {
+              doughnuts.doughnut.map(item=>
                   <div className="doughcon">
                     <div className="imgcon">
                     <Link to={'/' + item.name}><img src={item.img} className="doughimg"/></Link>
                     </div>
                     <div className="iconcon">
                       <span class="material-icons icon">add_shopping_cart</span>
-                      <Link to={'/' + item.name}><span className="doughname">{item.name}</span></Link>
+                      <Link to={'/' + item.name}>
+                        <span className="doughname">
+                          {item.name}
+                        </span>
+                      </Link>
                     </div>
                   </div>
                 )
-            } */}
+            }
             </div>
           </div>
         </div>
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,9 +1,9 @@
 import React, { createContext } from 'react'
-import whitesimg from '../img/white-sprinkle.jpg'
-import oldfimg from '../img/old-fashioned.jpg'
-import pnkfimg from '../img/pink-sprinkle.jpg'
-import sugarlyimg from '../img/sugarly.jpg'
-import doughballimg from '../img/dughball.jpg'
+import whitesimg from '../img/white-sprinkle.png'
+import oldfimg from '../img/old-fashioned.png'
+import pnkfimg from '../img/pink-sprinkle.png'
+import sugarlyimg from '../img/sugarly.png'
+import doughballimg from '../img/chocolate-glazed.png'
 
 export const ThemeContext = createContext();
 
@@ -13,12 +13,12 @@ class ThemeContextProvider extends React.Component {
     super();
     this.state = {
       doughnut: [
-        {name: 'whitesprinkle', img: whitesimg, inCart: false},
-        {name: 'oldfashioned', img: oldfimg, inCart: false},
-        {name: 'pinksprinkle', img: pnkfimg, inCart: false},
-        {name: 'sugarly', img: sugarlyimg, inCart: false},
-        {name: 'doughball', img: doughballimg, inCart: false},
-        {name: 'pinksprinkle', img: pnkfimg, inCart: false},
+        {name: 'Whitesprinkle', img: whitesimg, inCart: false},
+        {name: 'Oldfashioned', img: oldfimg, inCart: false},
+        {name: 'Pinksprinkle', img: pnkfimg, inCart: false},
+        {name: 'Sugarly', img: sugarlyimg, inCart: false},
+        {name: 'Doughball', img: doughballimg, inCart: false},
+        {name: 'Pinksprinkle', img: pnkfimg, inCart: false},
       ]
     }
   }
