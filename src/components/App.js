e7b33e3fbd21d11f9962e45a7a62dd8e828f9bf9
Yuriko Darragh <beljar731@gmail.com>
2020-07-06 11:59:12 +0900

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../styles/App.css';
 import Home from './Home'
-import Doughnut from './Doughnut'
+
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Route exact path='/' component={Home} />
-          <Route path='/:name' component={Doughnut} />
-        </Router>
+     <Router>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/:name' component={Doughnut}/>
+     </Router>
     </div>
   );
 }
