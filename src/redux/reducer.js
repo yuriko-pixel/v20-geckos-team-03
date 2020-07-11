74d51ae630433a3da719facbf05276ad41b5ba6d
yuriko-pixel <55519118+yuriko-pixel@users.noreply.github.com>
2020-07-11 17:18:02 +0900

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -34,24 +34,6 @@ const CartReducer = (state = initState, action) => {
       }
     }
 
-    // const newData = Object.assign([], { ...state });
-    // let item = newData.find((doughnut) => {
-    //   return doughnut.id == action.payload;
-    // });
-    // item.inCartSum += 1;
-    // newData[action.payload] = item;
-    // item = Object.assign([], { ...newData });
-    // return item;
-
-    // case "decrement":
-    //   const newData1 = Object.assign([], { ...state });
-    //   let item1 = newData1.find((doughnut) => {
-    //     return doughnut.id == action.payload;
-    //   });
-    //   item1.inCartSum += 1;
-    //   newData1[action.payload] = item;
-    //   item1 = Object.assign([], { ...newData1 });
-    //   return item1;
 
     case "FETCH_ITEMS":
       const fetchedItems = action.payload;
